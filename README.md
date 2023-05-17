a6d233419182e4522be73d8f40f1e0a4444d8f16
Nineteen991 <Nineteen99@gmail.com>
2023-05-16 23:55:23 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Simple business card that I can show people from my phone.
+
+QR Code leads to my portfolio website.
---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import me from './images/me.jpg'
+import qrcode from './images/qrcode.png'
+import './App.sass'
+
+export default function App() {
+  return (
+    <div className="business-card">
+      <img src={ me } className='my-portrait' alt='My portrait' />
+      <h1 className="title">Jason Alaniz</h1>
+      <h1 className="title">Web Developer</h1>
+      <a href='https://japortfolio.xyz/' className='website-link'>
+        <img src={ qrcode } className='qrcode' alt='My website' />
+      </a>
+    </div>
+  )
+}
