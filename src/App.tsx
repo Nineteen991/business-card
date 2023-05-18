d332fb2aede4552d5e36f31c436b860d14b28384
Nineteen991 <Nineteen99@gmail.com>
2023-05-17 19:11:43 -0700

    updated title

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ export default function App() {
     <div className="business-card">
       <img src={ me } className='my-portrait' alt='My portrait' />
       <h1 className="title">Jason Alaniz</h1>
-      <h1 className="title">Web Developer</h1>
+      <h1 className="title">Web3 Developer</h1>
       <a href='https://japortfolio.xyz/' className='website-link'>
         <img src={ qrcode } className='qrcode' alt='My website' />
       </a>
